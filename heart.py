1e8fdde16ce2c3a187dca193df885424cd0371d8
Chandru01061997 <csr.kolli55@gmail.com>
2021-08-18 16:18:43 +0530

    'FInalCommit'

--- heart.py
+++ heart.py
@@ -5,7 +5,7 @@ import pandas as pd
 import joblib
 
 app  = Flask(__name__)
-model = joblib.load('finalized_model.pkl')
+model = joblib.load('heart_failure_detect_model.pkl')
 
 @app.route("/")
 def form():
@@ -16,30 +16,32 @@ def heartfailuredetection():
 
     age = request.form.get('age')
     anaemia = request.form.get('anaemia')
+    CPK = request.form.get('creatinine_phosphokinase')
     diabetes = request.form.get('diabetes')
-    high_blood_pressure = request.form.get('high_blood_pressure')
+    ejection_fraction = request.form.get('ejection_fraction')
     platelets = request.form.get('platelets')
+    serum_creatinine = request.form.get('serum_creatinine')
     serum_sodium = request.form.get('serum_sodium')
-    time = request.form.get('time')
+    sex = request.form.get('sex')
     smoking = request.form.get('smoking')
+    time = request.form.get('time')
 
-    dic = {'anaemia':{'Yes':1, 'yes':1, 'No':0, 'no':0}, 'diabetes':{'Yes':1, 'yes':1, 'No':0, 'no':0}, 
-    'HBP':{'Yes':1, 'yes':1, 'No':0, 'no':0}, 'smoking':{'Yes':1, 'yes':1, 'No':0, 'no':0}}
+    dic = {'anaemia':{'Yes':1, 'No':0}, 'diabetes':{'Yes':1, 'No':0}, 'sex':{'Male':1, 'Female':0}, 'smoking':{'Yes':1, 'No':0}}
 
     anaemia = dic['anaemia'][anaemia]
     diabetes = dic['diabetes'][diabetes]
-    high_blood_pressure = dic['HBP'][high_blood_pressure]
+    sex = dic['sex'][sex]
     smoking = dic['smoking'][smoking]
 
-    prediction = model.predict([[int(age) , int(anaemia) , int(diabetes), int(high_blood_pressure), int(platelets), int(serum_sodium), int(time), int(smoking)]])
+    prediction = model.predict([[int(age) , int(anaemia) , int(CPK), int(diabetes), int(ejection_fraction), int(platelets), int(serum_creatinine), int(serum_sodium), int(sex), int(smoking), int(time)]])
 
     output = round(prediction[0] , 2)
     if output == 0:
-        predicted_result = "Survive"
+        predicted_result = "has chance of surviving"
     else:
-        predicted_result = "Not Survive"
+        predicted_result = "has a survival rate very low"
 
-    return render_template('heart.html' , final_output = f"The person will {predicted_result}")
+    return render_template('heart.html' , final_output = f"The person {predicted_result}")
 
 if __name__ == "__main__":
     app.run(debug=True)
